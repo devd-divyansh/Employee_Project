f2cae399dbe03032f30078ae15ca7f621e5ccfce
radhaTL <akhildivyansh90009@gmail.com>
2024-12-20 20:47:39 +0530

    commit by radhatl

---
+++ EmployeeProject/src/main/java/com/nt/EmployeesProjectApplication.java
@@ -0,0 +1,16 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesProjectApplication.class, args);
+	System.out.println("Welcome To Team");
+
+		
+	}
+
+}
